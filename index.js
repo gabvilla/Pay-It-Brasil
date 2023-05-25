b2b1f80dde3ff058f2ec7bf7cc28fe0d86abc645
gabvilla <gabriel.villassanti2@hotmail.com>
2023-05-25 17:38:04 -0300

    Atualização Carrossel

---
+++ index.js
@@ -0,0 +1,35 @@
+/*De primeiro momento, precisamos selecionar os controles e os slides*/
+const controls = document.querySelectorAll('button');
+let currentItem = 0;
+const slides = document.querySelectorAll('slide');
+const maxSlides = slides.length;
+
+/*Depois, testamos se o controle clickado é para a esquerda*/
+controls.forEach((button) => {
+   button.addEventListener('click', (e) => {
+      isLeft = e.target.classList.contains('button-left');
+/*Caso sim, nosso índice é alterado em -1*/
+      if (isLeft){
+         currentItem -= 1;
+      } else{
+         currentItem += 1;
+      }
+/*Caso esteja no último índice, ele retorna para o primeiro e vice-versa*/
+      if (currentItem >= maxSlides){
+         currentItem = 0;
+      }
+
+      if (currentItem < 0){
+         currentItem = maxSlides - 1
+      }
+
+/*Nesse passo, removemos a classe de slide atual*/
+      slides.forEach((slide) =>{slide.classList.remove('current-slide')});
+
+      slides[currentItem].scrollIntoView({
+         inline: 'center',
+         behavior: 'auto',
+      })
+      slides[currentItem].classList.add('current-slide')
+   })
+})
