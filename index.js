49231f2c401e0b497629a6817e15665c0b6e29e3
gabvilla <gabriel.villassanti2@hotmail.com>
2023-05-27 19:47:29 -0300

    alteração no carrossel dinâmico

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 /*De primeiro momento, precisamos selecionar os controles e os slides*/
 const controls = document.querySelectorAll('button');
 let currentItem = 0;
-const slides = document.querySelectorAll('slide');
+let currentDot = 0;
+const dots = document.querySelectorAll('div.dots');
+const slides = document.querySelectorAll('div.slide');
 const maxSlides = slides.length;
+const maxDots = dots.length
 
 /*Depois, testamos se o controle clickado é para a esquerda*/
 controls.forEach((button) => {
@@ -11,25 +14,32 @@ controls.forEach((button) => {
 /*Caso sim, nosso índice é alterado em -1*/
       if (isLeft){
          currentItem -= 1;
+         currentDot -= 1;
       } else{
          currentItem += 1;
+         currentDot += 1;
       }
 /*Caso esteja no último índice, ele retorna para o primeiro e vice-versa*/
       if (currentItem >= maxSlides){
          currentItem = 0;
+         currentDot = 0;
       }
 
       if (currentItem < 0){
-         currentItem = maxSlides - 1
+         currentItem = maxSlides - 1;
+         currentDot = maxDots -1;
       }
 
 /*Nesse passo, removemos a classe de slide atual*/
       slides.forEach((slide) =>{slide.classList.remove('current-slide')});
+
+      dots.forEach((dot) => {dot.classList.remove('current-dot')});
 
       slides[currentItem].scrollIntoView({
          inline: 'center',
          behavior: 'auto',
       })
       slides[currentItem].classList.add('current-slide')
+      dots[currentDot].classList.add('current-dot')
    })
 })
